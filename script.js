541676c379ac2c912fda373d810cc87e0b2792ee
Danielle Cristina <dnaielle.trybe@gmail.com>
2021-11-24 13:54:49 -0300

    Requisitos 7, 8 e 9

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 //Cria Lista de Tarefas
-function addTarefa(){
-  const textTarefa = document.querySelector('#texto-tarefa').value;
-  const liTarefa = document.createElement('li');
-  
-  liTarefa.innerText = textTarefa;
+function addTarefa() {
+  if (document.querySelector('#texto-tarefa').value !== '') {
+    const textTarefa = document.querySelector('#texto-tarefa').value;
+    const liTarefa = document.createElement('li');
+    liTarefa.innerText = textTarefa;
 
-  document.getElementById('lista-tarefas').appendChild(liTarefa);
-
+    document.getElementById('lista-tarefas').appendChild(liTarefa);
+  }
   document.querySelector('#texto-tarefa').value = '';
 }
+
+//Seleciona item da lista de tarefas
+const listaTarefa = document.querySelector('#lista-tarefas');
+listaTarefa.addEventListener('click', selectTarefa);
+
+function selectTarefa(event) {
+  let item = document.querySelector('.selected');
+  event.target.classList.add('selected');
+
+  if (item) {
+    item.classList.remove('selected');
+  }
+}
+
+//defini item como concluído
+const dblItem = document.querySelector('#lista-tarefas');
+dblItem.addEventListener('dblclick', itemComplet);
+
+function itemComplet(event) {
+  let itemdbl = document.querySelector('.completed');
+  event.target.classList.add('completed');
+
+  if (itemdbl) {
+    itemdbl.classList.remove('completed')
+  }
+} 
